5082d784a447f1ec1e88aba45d5cc9202164f00f
Erik Schleiff <devnull@localhost>
2007-06-20 12:02:19 +0000

    added Constants struct for project specific constants.

---
+++ src/Constants.h
@@ -0,0 +1,18 @@
+#ifndef BV3D_CONSTANTS
+#define BV3D_CONSTANTS
+
+namespace BV3D
+{
+	struct Constants
+	{
+		enum BV3D_TEAM { BV3D_TEAM1, BV3D_TEAM2 };
+		
+		enum BV3D_ERROR
+		{
+			BV3D_NO_ERROR,
+			BV3D_INVALID_PARAMETER
+		};
+	};
+};
+
+#endif	// BV3D_CONSTANTS
