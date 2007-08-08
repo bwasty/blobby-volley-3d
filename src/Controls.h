90e5f4a24b8409230c2c471a6c764d02bdb7d87e
Erik Schleiff <devnull@localhost>
2007-08-08 13:05:02 +0000

    rewrote comments in Controls.h

--- src/Controls.h
+++ src/Controls.h
@@ -1,21 +1,13 @@
 /**
  * Controls represents the abstract super class for handling user input for Blobbs.
- * it declares the following interface methods:
- * - virtual void processInput(InputEvent*):
- *   for Blobbs to pass InputEvents to the Controls
- * - virtual bool isRequested(COMMAND):
- *   for Blobbs to test if any commands are issued
- * - virtual void consumed():
- *   for Blobbs to notify the Controls that the commands for the current frame were evaluated.
- *   this is necessary for mouse controls, where requests cannot be evaluated by the state of keys.
- *   mouse movement requests are therefore kept until consumed() is called each frame.
  *
  * the primary intent of the Controls is to allow tracking of multiple keys.
  * since for example if more than one key one the keyboard is held down, only the last one of
  * the keys will be periodically reported to the system. still, any messages regarding initial key pressing
- * and releasing should be captured. therefor, if a key was pressed that is bound to command, the Controls
- * sets the corresponding command until the key is released. Blobbs query the commands from the Controls
- * when they are updated.
+ * and releasing should be captured. therefor, if a key was pressed that is bound to a command, the Controls
+ * sets the corresponding command until the key is released.
+ *
+ * Blobbs query the commands from the Controls when they are updated.
  */
 
 #ifndef BV3D_CONTROLS
@@ -28,14 +20,41 @@
 
 namespace BV3D
 {
-	class Controls : public VRS::SharedObj{
+	class Controls : public VRS::SharedObj {
+
 	public:
+		/**
+		 * listing of possible (abstract) input commands
+		 */
 		enum COMMAND {FORWARD=0, BACKWARD=1, RIGHT=2, LEFT=3, JUMP=4};
-	public:
+
+	protected:
+		/**
+		 * ctor
+		 */
 		Controls() {}
+	public:
+		/**
+		 * dtor
+		 */
 		virtual ~Controls() {}
+
+		/**
+		 * is a callback for processing InputEvent's
+		 */
 		virtual void processInput(VRS::SO<VRS::InputEvent> ie) = 0;
+
+		/**
+		 * is called by Blobb to check if a certain input command was issued.
+		 * blobbs should move accordingly
+		 * \param cmd is one of the possible input commands of enum BV3D::Controls::COMMAND
+		 */
 		virtual bool isRequested(COMMAND cmd) = 0;
+
+		/**
+		 * is called by Blobb to signal that the input for the current frame was acknowlegded.
+		 * (this is necessary ie for MouseControls)
+		 */
 		virtual void consumed() = 0;
 	};
 }
