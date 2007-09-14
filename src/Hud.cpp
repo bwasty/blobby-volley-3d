25864c923766b0d94774ac39d4a8730abc35493d
erik.schleiff <devnull@localhost>
2007-09-14 13:39:06 +0000

    commented HUD.

--- src/Hud.cpp
+++ src/Hud.cpp
@@ -13,56 +13,91 @@
 #include <stdio.h>
 
 BV3D::HUD::HUD(VRS::Color colorPlayer1, VRS::Color colorPlayer2) {
-	setColors(colorPlayer1, colorPlayer2);
-
-	mScene =  new VRS::OverlayImageGL();
-	VRS::SO<VRS::OverlayImageGL> overlay = VRS_Cast(VRS::OverlayImageGL, mScene);
-
+	// set default indices
 	mScoreP1Index = -1;
 	mScoreP2Index = -1;
 	mServingIndex = -1;
 	mWinnerIndex = -1;
+
+	mScene = new VRS::OverlayImageGL();
+
+	setColors(colorPlayer1, colorPlayer2);
 }
 
+
+/**
+ * set team colors for score display
+ * (to actually refresh the HUD setScore() needs to be called)
+ */
 void BV3D::HUD::setColors(VRS::Color colorPlayer1, VRS::Color colorPlayer2) {
 	// store colors internally without transparency
 	mP1Color = VRS::Color(colorPlayer1[0],colorPlayer1[1],colorPlayer1[2]);
 	mP2Color = VRS::Color(colorPlayer2[0],colorPlayer2[1],colorPlayer2[2]);
 }
 
+
+/**
+ * displays the specified score in the HUD and indicates the server
+ * \param scoreP1 specifies the score to be displayed for player 1
+ * \param scoreP2 specifies the score to be displayed for player 2
+ * \param servingTeam specifies the serving team to be indicated on display
+ */
 void BV3D::HUD::setScore(int scoreP1, int scoreP2, TEAM servingTeam) {
 	VRS::SO<VRS::OverlayImageGL> overlay = VRS_Cast(VRS::OverlayImageGL, mScene);
-	if(mScoreP1Index>=0) overlay->removeOverlayItem(mScoreP1Index);
-	if(mScoreP2Index>=0) overlay->removeOverlayItem(mScoreP2Index);
-	if(mServingIndex>=0) overlay->removeOverlayItem(mServingIndex);
-	if(mWinnerIndex>=0) overlay->removeOverlayItem(mWinnerIndex);
 
-	// set up player 1 score
+	// remove current HUD items
+	if(mScoreP1Index>=0)
+		overlay->removeOverlayItem(mScoreP1Index);
+	if(mScoreP2Index>=0)
+		overlay->removeOverlayItem(mScoreP2Index);
+	if(mServingIndex>=0)
+		overlay->removeOverlayItem(mServingIndex);
+	if(mWinnerIndex>=0)
+		overlay->removeOverlayItem(mWinnerIndex);
+
+	// construct player 1 score string and add text to the overlay
 	char buffer[5];
 	sprintf_s(buffer,4, "%d",scoreP1);
-	mScoreP1Index = overlay->addText(VRS::Font::searchFont("Arial Black (TrueType)", VRS::Font::TEXTURE, 150),
-		buffer,VRS::Vector(0.2,0.95),VRS::Vector(0.35,1.0),VRS::Text::LEFT,VRS::Text::TOP,mP1Color);
+	mScoreP1Index = overlay->addText(
+		VRS::Font::searchFont("Arial Black (TrueType)", VRS::Font::TEXTURE, 150), buffer,
+		VRS::Vector(0.2,0.95),VRS::Vector(0.35,1.0),VRS::Text::LEFT,VRS::Text::TOP,mP1Color);
 
-	// set up player 2 score
+	// construct player 2 score string and add text to the overlay
 	sprintf_s(buffer,4, "%d",scoreP2);
-	mScoreP2Index = overlay->addText(VRS::Font::searchFont("Arial Black (TrueType)", VRS::Font::TEXTURE, 150),
-		buffer,VRS::Vector(0.65,0.95),VRS::Vector(0.8,1.0),VRS::Text::LEFT,VRS::Text::TOP,mP2Color);
+	mScoreP2Index = overlay->addText(
+		VRS::Font::searchFont("Arial Black (TrueType)", VRS::Font::TEXTURE, 150), buffer,
+		VRS::Vector(0.65,0.95),VRS::Vector(0.8,1.0),VRS::Text::LEFT,VRS::Text::TOP,mP2Color);
 
 	// set up serving indicator
-	VRS::Vector servingMin, servingMax;
-	if(servingTeam==TEAM1) {
-		servingMin = VRS::Vector(0.10,0.95); servingMax = VRS::Vector(0.15,1.0);}
-	else if(servingTeam==TEAM2) {
-		servingMin = VRS::Vector(0.85,0.95); servingMax = VRS::Vector(0.9,1.0);}
-	mServingIndex = overlay->addText(VRS::Font::searchFont("Arial Black (TrueType)", VRS::Font::TEXTURE, 150),
-		"!",servingMin,servingMax,VRS::Text::LEFT,VRS::Text::TOP,VRS::Color(0.8,0.0,0.0));
+	VRS::Vector servingMin, servingMax;	// left-lower and right-upper corner for indicator
+	if(servingTeam==TEAM1) {	// display indicator to the left of player 1 score
+		servingMin = VRS::Vector(0.10,0.95); servingMax = VRS::Vector(0.15,1.0);
+	}
+	else if(servingTeam==TEAM2) {	// display indicator to the left of player 2 score
+		servingMin = VRS::Vector(0.85,0.95); servingMax = VRS::Vector(0.9,1.0);
+	}
+	mServingIndex = overlay->addText(
+		VRS::Font::searchFont("Arial Black (TrueType)", VRS::Font::TEXTURE, 150), "!",
+		servingMin,servingMax, VRS::Text::LEFT, VRS::Text::TOP, VRS::Color(0.8,0.0,0.0));
 }
 
+
+/**
+ * display message informing about winner of the game
+ */
 void BV3D::HUD::showWinner(TEAM winner) {
+	// construct message
 	char buffer[15];
 	sprintf_s(buffer,15, "Player %d wins!",(int)winner+1);
+
 	VRS::SO<VRS::OverlayImageGL> overlay = VRS_Cast(VRS::OverlayImageGL, mScene);
-	if(mWinnerIndex>=0) overlay->removeOverlayItem(mWinnerIndex);
-	mWinnerIndex = overlay->addText(VRS::Font::searchFont("Arial Black (TrueType)", VRS::Font::TEXTURE, 150),
-		buffer,VRS::Vector(0.1,0.45),VRS::Vector(0.9,0.65),VRS::Text::LEFT,VRS::Text::CENTER_V,VRS::Color(0.0,0.0,1.0));
+
+	if(mWinnerIndex>=0)					// remove previous message if necessary
+		overlay->removeOverlayItem(mWinnerIndex);
+
+	// add message to overlay using winner color
+	mWinnerIndex = overlay->addText(
+		VRS::Font::searchFont("Arial Black (TrueType)", VRS::Font::TEXTURE, 150), buffer,
+		VRS::Vector(0.1,0.45), VRS::Vector(0.9,0.65), VRS::Text::LEFT, VRS::Text::CENTER_V,
+		(winner) ? mP2Color : mP1Color);
 }
