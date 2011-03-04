599670cca343c9012d6b6f559742cb2eac3999cb
karan <devnull@localhost>
2011-03-04 21:38:26 +0100

    created Android project

---
+++ AndroidVersion/src/de/bv3d/BlobbyVolleyMain.java
@@ -0,0 +1,18 @@
+// basis for this class taken from http://code.google.com/p/opengles-book-samples/ 
+// Ch8_SimpleVertexShader
+package de.bv3d;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+public class BlobbyVolleyMain extends Activity {
+	private GLSurfaceView mGLSurfaceView;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main); // TODO: remove main layout?
+    }
+}
