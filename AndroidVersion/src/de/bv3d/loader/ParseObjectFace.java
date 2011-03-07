c3f62891025b09a1f44f616224ebd70ea5868341
karan <devnull@localhost>
2011-03-07 15:18:39 +0100

    added 3ds loader from min3d (unchanged yet) and Blobb1.3ds

---
+++ AndroidVersion/src/de/bv3d/loader/ParseObjectFace.java
@@ -0,0 +1,17 @@
+package de.bv3d.loader;
+
+/**
+ * orginally taken from min3d - http://code.google.com/p/min3d/
+ * 
+ * changed by Benjamin Wasty
+ */
+public class ParseObjectFace {
+	public int[] v;
+	public int[] uv;
+	public int[] n;
+	public int faceLength;
+	public boolean hasuv;
+	public boolean hasn;
+	public String materialKey;
+
+}
